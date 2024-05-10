a7060388c23c49e4afc39fce56e58236e8ccbdb6
mshahria <122585062+mshahria@users.noreply.github.com>
2024-05-10 18:03:55 -0500

    Update __init__.py

--- SaveData/__init__.py
+++ SaveData/__init__.py
@@ -1,18 +1,33 @@
 import azure.functions as func
 import json
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, outputDocument: func.Out[func.Document]) -> func.HttpResponse:
     try:
-        # Parse the JSON data from the request
         data = req.get_json()
-        # Echo the received data back as a JSON response
-        return func.HttpResponse(json.dumps({
-            "status": "Received",
-            "data": data
-        }), mimetype="application/json", status_code=200)
+        # Create a new document
+        doc = func.Document.from_dict(data)
+        outputDocument.set(doc)
+        return func.HttpResponse("Document stored", status_code=200)
     except ValueError:
-        # If there is a JSON parsing error, return an error response
         return func.HttpResponse("Invalid JSON", status_code=400)
+
+
+
+# import azure.functions as func
+# import json
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     try:
+#         # Parse the JSON data from the request
+#         data = req.get_json()
+#         # Echo the received data back as a JSON response
+#         return func.HttpResponse(json.dumps({
+#             "status": "Received",
+#             "data": data
+#         }), mimetype="application/json", status_code=200)
+#     except ValueError:
+#         # If there is a JSON parsing error, return an error response
+#         return func.HttpResponse("Invalid JSON", status_code=400)
 
 # import azure.functions as func
 # import os
