74051b3c070260413a094a1b2963dd746db3f462
mshahria <122585062+mshahria@users.noreply.github.com>
2024-06-25 12:47:41 -0500

    Update __init__.py

--- SaveData/__init__.py
+++ SaveData/__init__.py
@@ -25,7 +25,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         # Access the container based on user_id and insert data
         container = database.get_container_client(user_id)
-        container.upsert_item(store_data)
+        # container.upsert_item(store_data)
 
         return func.HttpResponse(
             json.dumps({"status": "Data stored successfully", "userId": user_id, "data": store_data}),
